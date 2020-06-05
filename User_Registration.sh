9d374d27ea054af97099975ce020969d7401c634
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 17:43:31 +0530

    First Name

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,1 +1,20 @@
+#!/bin/bash -x
+
 echo Welcome to User Registration
+
+function check()
+{
+        i=$1
+        j=$2
+        if [[ $i =~ $j ]]
+        then
+                echo Saved
+        else
+                echo Invalid Input
+        fi
+}
+name="^[A-Z][A-Za-z0-9]{2,}$"
+echo -e "Enter your valid First name \n:- First name should start with Cap and has minimum 3 characters"
+read fname
+check $fname $name
+
