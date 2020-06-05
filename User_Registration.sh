cb6f2c57092c24c16868b626c75befe211fa2d02
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 17:58:48 +0530

    Name, Email and Phone Number

--- User_Registration.sh
+++ User_Registration.sh
@@ -15,6 +15,7 @@ function check()
 }
 name="^[A-Z][A-Za-z0-9]{2,}$"
 email="^[0-9a-zA-Z]+[.]*[0-9a-zA-z]*[@][a-zA-Z]+([.][a-zA-Z]+){1,2}$"
+pno="^[0-9]{2}[ ][0-9]{10}$"
 echo -e "Enter your valid First name \n:- First name should start with Cap and has minimum 3 characters"
 read fname
 check $fname $name
@@ -23,3 +24,6 @@ read lname
 check $lname $name
 read -p "Enter your valid email Id  " eId
 check $eId $email
+echo "Enter your valid mobile number as Country code followd by space and 10 digit number "
+read num
+check "$num" "$pno"
