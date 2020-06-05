0a5aabb9da8e65cabc30da7afd7da661976d4399
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 18:03:23 +0530

    User Registration Form

--- User_Registration.sh
+++ User_Registration.sh
@@ -16,6 +16,7 @@ function check()
 name="^[A-Z][A-Za-z0-9]{2,}$"
 email="^[0-9a-zA-Z]+[.]*[0-9a-zA-z]*[@][a-zA-Z]+([.][a-zA-Z]+){1,2}$"
 pno="^[0-9]{2}[ ][0-9]{10}$"
+pass="^[(A-Z)+(a-z)*(0-9)+(!@#$&+=*/_%^~){1}]{8,}$"
 echo -e "Enter your valid First name \n:- First name should start with Cap and has minimum 3 characters"
 read fname
 check $fname $name
@@ -26,4 +27,5 @@ read -p "Enter your valid email Id  " eId
 check $eId $email
 echo "Enter your valid mobile number as Country code followd by space and 10 digit number "
 read num
-check "$num" "$pno"
+check "$num" "$pno"                                                                                                                                                                                                echo -e "Enter the password and fallow the rules: \n1: minimum 8 Characters \n2: Should have at least 1 Upper Case \n3: Should have at least 1 numeric number in the password \n4: Has exactly 1 Special Character"read password
+check $password $pass
